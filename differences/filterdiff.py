ad2af9f5e80f8b684cd2be9062d0015f2e030cbd
nevesnunes <>
2021-02-04 13:27:25 +0000

    filterdiff.py: Use unified format

--- differences/filterdiff.py
+++ differences/filterdiff.py
@@ -1,52 +1,92 @@
 #!/usr/bin/env python3
 
-import diff_match_patch
+import difflib
 import sys
 import re
 
-
-def unified_format(diff):
-    changes = []
-    change_symbol = None
-    has_changes = False
-    for pair in diff:
-        if pair[0] == -1:
-            change_symbol = "-"
-            has_changes = True
-        elif pair[0] == 1:
-            change_symbol = "+"
-            has_changes = True
-        elif pair[0] == 0:
-            change_symbol = ""
-        changes.append(f"{change_symbol}{pair[1]}")
-    if has_changes:
-        return changes
+DEBUG = False
+REPLACE_PREFIX = "__027b596b_2b2b_451e_b051_2130237b863f__{}__"
 
 
-with open(sys.argv[1], "r") as f:
-    rules = f.readlines()
+def debug(*args):
+    if DEBUG:
+        print(args)
 
-patterns = []
-for rule in rules:
-    patterns.append(re.compile(rule.strip(), re.IGNORECASE))
 
-with open(sys.argv[2], "r") as f1, open(sys.argv[3], "r") as f2:
-    c1 = f1.readlines()
-    c2 = f2.readlines()
-
-dmp = diff_match_patch.diff_match_patch()
-for l1, l2 in [(x.strip(), y.strip()) for x, y in zip(c1, c2)]:
-    diff = dmp.diff_main(l1, l2)
-    dmp.diff_cleanupSemantic(diff)
-    changes = unified_format(diff)
-    if changes:
-        print(changes)
-
+def compute_replacements(patterns, contents):
+    replace_counter = 0
+    replacements = {}
     for pattern in patterns:
-        l1 = re.sub(pattern, "_", l1)
-        l2 = re.sub(pattern, "_", l2)
-    diff = dmp.diff_main(l1, l2)
-    dmp.diff_cleanupSemantic(diff)
-    changes = unified_format(diff)
-    if changes:
-        print(changes, file=sys.stderr)
+        for i, c in enumerate(contents):
+            text_to_replace = c
+            for match in re.finditer(pattern, c):
+                if not text_to_replace:
+                    break
+
+                matched_group = match.groups()[0]
+                if pattern not in replacements:
+                    replace_str = REPLACE_PREFIX.format(replace_counter)
+                    replace_counter += 1
+                    replacements[pattern] = {
+                        "replace_str": replace_str,
+                        "matched_strs": {},
+                    }
+                replace_str = replacements[pattern]["replace_str"]
+                if i not in replacements[pattern]["matched_strs"]:
+                    replacements[pattern]["matched_strs"][i] = []
+                replacements[pattern]["matched_strs"][i].append(matched_group)
+
+                text_to_replace = re.sub(pattern, replace_str, text_to_replace, 1)
+
+                replace_str_index = text_to_replace.index(replace_str)
+                text_to_replace = text_to_replace[
+                    replace_str_index + len(replace_str) :
+                ]
+    debug(f"replacements: {replacements}")
+    return replacements
+
+
+def apply_replacements(replacements, contents):
+    replaced_texts = []
+    for i, c in enumerate(contents):
+        replaced_text = c
+        for k, v in replacements.items():
+            for matched_str in v["matched_strs"][i]:
+                debug(matched_str, v["replace_str"], replaced_text)
+                replaced_text = re.sub(matched_str, v["replace_str"], replaced_text, 1)
+        replaced_texts.append(replaced_text)
+    debug(f"replaced_texts: {replaced_texts}")
+    return replaced_texts
+
+
+if __name__ == "__main__":
+    patterns_filename = sys.argv[1]
+    text1_filename = sys.argv[2]
+    text2_filename = sys.argv[3]
+
+    patterns = []
+    with open(patterns_filename, "r") as f:
+        rules = f.readlines()
+        for rule in rules:
+            patterns.append(re.compile(rule.strip(), re.IGNORECASE | re.MULTILINE))
+
+    with open(text1_filename, "r") as f1, open(text2_filename, "r") as f2:
+        c1 = f1.read().strip()
+        c2 = f2.read().strip()
+
+    contents = [c1, c2]
+    replacements = compute_replacements(patterns, contents)
+    replaced_texts = apply_replacements(replacements, contents)
+
+    diffs = difflib.unified_diff(
+        replaced_texts[0].split("\n"),
+        replaced_texts[1].split("\n"),
+        fromfile=sys.argv[2],
+        tofile=sys.argv[3],
+    )
+    for diff in diffs:
+        for k, v in replacements.items():
+            for i in range(2):
+                for matched_str in v["matched_strs"][i]:
+                    diff = re.sub(v["replace_str"], matched_str, diff, 1)
+        print(diff.rstrip())
