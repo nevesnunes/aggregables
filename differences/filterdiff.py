2739b7846dee704d344e4ea1507dc3e50c1b908a
nevesnunes <9061071+nevesnunes@users.noreply.github.com>
2021-02-24 09:11:35 +0000

    Update filterdiff

--- differences/filterdiff.py
+++ differences/filterdiff.py
@@ -1,47 +1,75 @@
 #!/usr/bin/env python3
 
 import difflib
-import sys
+import os
 import re
+import sys
 
-DEBUG = False
-REPLACE_PREFIX = "__027b596b_2b2b_451e_b051_2130237b863f__{}__"
+DEBUG = bool(os.environ.get("DEBUG"))
+EXACT = bool(os.environ.get("EXACT"))
+if EXACT:
+    REPLACE_STR = "__027b596b_2b2b_451e_b051_2130237b863f__{}__"
+else:
+    REPLACE_STR = "\x00"
 
 
-def debug(*args):
+def debug(*args, indent=0):
     if DEBUG:
-        print(args)
+        print(" " * indent, args)
 
 
 def compute_replacements(patterns, texts):
     replace_counter = 0
     replacements = {}
-    for pattern in patterns:
-        for i, c in enumerate(texts):
-            text_to_replace = c
-            for match in re.finditer(pattern, c):
-                if not text_to_replace:
-                    break
+    pattern_replace_strs = {}
+    for i, c in enumerate(texts):
+        previous_text = c
+        text_to_replace = c
+        needs_replace = True
+        while needs_replace:
+            if not text_to_replace:
+                break
 
-                matched_group = match.groups()[0]
-                if pattern not in replacements:
-                    replace_str = REPLACE_PREFIX.format(replace_counter)
+            next_group = None
+            next_match = None
+            next_match_start = float("inf")
+            next_pattern = None
+            for pattern in patterns:
+                debug(pattern, text_to_replace, indent=4)
+                maybe_match = re.search(pattern, text_to_replace)
+                debug(maybe_match, indent=8)
+                if maybe_match and maybe_match.start() < next_match_start:
+                    next_group = maybe_match.groups()[0]
+                    next_match_start = maybe_match.start()
+                    next_match = maybe_match
+                    next_pattern = pattern
+            if not next_match:
+                break
+
+            if EXACT:
+                if pattern not in pattern_replace_strs:
+                    replace_str = REPLACE_STR.format(replace_counter)
                     replace_counter += 1
-                    replacements[pattern] = {
-                        "replace_str": replace_str,
-                        "matched_strs": {},
-                    }
-                replace_str = replacements[pattern]["replace_str"]
-                if i not in replacements[pattern]["matched_strs"]:
-                    replacements[pattern]["matched_strs"][i] = []
-                replacements[pattern]["matched_strs"][i].append(matched_group)
+                    pattern_replace_strs[pattern] = replace_str
+                replace_str = pattern_replace_strs[pattern]
+            else:
+                replace_str = REPLACE_STR * len(next_group)
 
-                text_to_replace = re.sub(pattern, replace_str, text_to_replace, 1)
+            if i not in replacements:
+                replacements[i] = []
+            replacements[i].append(
+                {
+                    "group": next_group,
+                    "pattern": next_pattern,
+                    "replace_str": replace_str,
+                }
+            )
+            debug(i, text_to_replace, next_pattern, next_group, replace_str, indent=4)
 
-                replace_str_index = text_to_replace.index(replace_str)
-                text_to_replace = text_to_replace[
-                    replace_str_index + len(replace_str) :
-                ]
+            text_to_replace = text_to_replace[next_match.end() :]
+            if text_to_replace == previous_text or not text_to_replace:
+                needs_replace = False
+            previous_text = text_to_replace
     debug(f"replacements: {replacements}")
     return replacements
 
@@ -49,11 +77,24 @@ def compute_replacements(patterns, texts):
 def apply_replacements(replacements, texts):
     replaced_texts = []
     for i, c in enumerate(texts):
-        replaced_text = c
-        for k, v in replacements.items():
-            for matched_str in v["matched_strs"][i]:
-                debug(matched_str, v["replace_str"], replaced_text)
-                replaced_text = re.sub(matched_str, v["replace_str"], replaced_text, 1)
+        text_to_replace = c
+        replaced_text = ""
+        if i in replacements:
+            for match_dict in replacements[i]:
+                match = re.search(match_dict["pattern"], text_to_replace)
+                replaced_end = match.start() + len(match_dict["replace_str"])
+                replaced_chunk = re.sub(
+                    match_dict["group"], match_dict["replace_str"], text_to_replace, 1
+                )
+                debug(f"replaced_chunk: {replaced_chunk}", indent=4)
+                debug(replaced_chunk[replaced_end:], indent=8)
+                debug(replaced_chunk[:replaced_end], indent=8)
+                replaced_text += replaced_chunk[:replaced_end]
+                text_to_replace = replaced_chunk[replaced_end:]
+                if not text_to_replace:
+                    break
+        # include last chunk that wasn't matched
+        replaced_text += text_to_replace
         replaced_texts.append(replaced_text)
     debug(f"replaced_texts: {replaced_texts}")
     return replaced_texts
@@ -73,15 +114,31 @@ def compute_diffs(rules, text1, text2):
         replaced_texts[1].split("\n"),
         fromfile="base",
         tofile="derivative",
+        n=max(len(replaced_texts[0]), len(replaced_texts[1])),
     )
-    replaced_diffs = []
+    text1_lines = text1.split("\n")
+    text2_lines = text2.split("\n")
+    new_text_lines = []
+    text1_pos = 0
+    text2_pos = 0
+    for i in range(3):
+        next(diffs)
     for diff in diffs:
-        for k, v in replacements.items():
-            for i in range(2):
-                for matched_str in v["matched_strs"][i]:
-                    diff = re.sub(v["replace_str"], matched_str, diff, 1)
-            replaced_diffs.append(diff.rstrip())
-    return replaced_diffs
+        if diff[0] == " ":
+            new_text_lines.append(text1_lines[text1_pos])
+            text1_pos += 1
+            text2_pos += 1
+        elif diff[0] == "-":
+            text1_pos += 1
+        elif diff[0] == "+":
+            new_text_lines.append(text2_lines[text2_pos])
+            text2_pos += 1
+
+    new_text = "\n".join(new_text_lines)
+    diffs = difflib.unified_diff(
+        text1.split("\n"), new_text.split("\n"), fromfile="base", tofile="derivative",
+    )
+    return list(map(lambda x: x.rstrip(), diffs))
 
 
 if __name__ == "__main__":
