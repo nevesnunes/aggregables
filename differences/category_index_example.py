2c51734ceec1ae7e812ef90ee092d1dbe911f8cb
nevesnunes <>
2021-03-12 00:03:38 +0000

    funcdiff: filter entries by search input

---
+++ differences/category_index_example.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from category_index import ReverseIndex
+
+DATA = [
+    {
+        "title": "Django",
+        "description": "Django is a high-level Python Web framework that "
+        "encourages rapid development and clean, pragmatic design.  Built by "
+        "experienced developers, it takes care of much of the hassle of Web "
+        "development, so you can focus on writing your app without needing to "
+        "reinvent the wheel. It’s free and open source.",
+    },
+    {
+        "title": "Python",
+        "description": "Python is a programming language that lets you work "
+        "more quickly and integrate your systems more effectively.",
+    },
+]
+SYNONYMS = {
+    "rapid": "quick",
+}
+index = ReverseIndex(DATA, SYNONYMS, "title", "description")
+print(index.index)
+print(list(index.query("Python")))
+print(list(index.query("Python", fields=["title"])))
+print(list(index.query("python", fields=["description"])))
+print(list(index.query("Python web")))
+print(list(index.query("Python web", "OR")))
+print(list(index.query("quick")))
+print(list(index.query("rapid")))
+print(list(index.query("of")))
